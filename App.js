aa9a3939195109514bbc5458c652cc31a390e33e
mdvitalie.ie <mdvitalie@gmail.com>
2023-05-25 17:17:10 +0100

    update on Displaying Items in a Grid

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import CategoriesScreen from './screens/CategoriesScreen';
 export default function App() {
   return (
     <>
-      <StatusBar style="auto"  />
+      <StatusBar style="light"  />
       <CategoriesScreen/>
     </>
     
