66aec03f27eee7d55a9ca22eb438f6b7e7326dfa
mdvitalie.ie <mdvitalie@gmail.com>
2023-05-26 12:31:16 +0100

    Added Images & Styling to Meals

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,12 +1,68 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable, Image, StyleSheet, Platform } from "react-native";
+import React from "react";
 
-const MealItem = ({title}) => {
+const MealItem = ({ title, imageUrl, duration, complexity, affordability }) => {
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable android_ripple={{color: '#ccc'}}
+        style={({ pressed }) => pressed ? styles.buttonPressed : null}
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image source={{ uri: imageUrl }} style={styles.image} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.ditalItem}>{duration}m</Text>
+            <Text style={styles.ditalItem}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.ditalItem}>{affordability.toUpperCase()}</Text>
+          </View>
+        </View>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default MealItem
+export default MealItem;
+
+const styles = StyleSheet.create({
+  mealItem:{
+    margin: 16,
+    borderRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    overflow: "hidden",
+    backgroundColor: '#fff',
+    elevation: 4,
+    shadowColor: '#000',
+    shadowOpacity: 0.35,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 16,
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden"
+  },
+  image:{
+    width: '100%',
+    height: 200
+  },
+  title:{
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8
+  },
+  details:{
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 8,
+    justifyContent: "center"
+  },
+  ditalItem: {
+    marginHorizontal: 5,
+    fontSize: 12
+  },
+   buttonPressed: {
+    opacity: 0.5,
+  },
+});
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -11,7 +11,17 @@ const MealsOverViewScreen = ({ route }) => {
   });
 
   function renderMealItem(itemData) {
-    return <MealItem title={itemData.item.title} />
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration
+    }
+    // return <MealItem title={itemData.item.title} imageUrl={itemData.item.imageUrl} />
+    return <MealItem {...mealItemProps} />
   }
 
   return (
