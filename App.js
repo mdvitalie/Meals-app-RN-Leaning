aa4d8aaf216a8c86b1c67fef1dccbc60815c4085
mdvitalie.ie <mdvitalie@gmail.com>
2023-05-27 12:19:33 +0100

    Setting Navigation Options Dynamically, Styling Screen Headers

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet} from 'react-native';
-import CategoriesScreen from './screens/CategoriesScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import {createNativeStackNavigator } from '@react-navigation/native-stack'
-import MealsOverViewScreen from './screens/MealsOverviewScreen';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MealsOverViewScreen from "./screens/MealsOverviewScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="dark" />
+      <StatusBar style="light" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen}/>
-          <Stack.Screen name="MealsOverview" component={MealsOverViewScreen}/>
+        <Stack.Navigator screenOptions={{
+          headerStyle: {backgroundColor: '#351401'},
+              headerTintColor: '#fff',
+              contentStyle: {backgroundColor: '#3f2f25'}
+        }}>
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{ 
+              title: "All Categories",
+            }}
+          />
+          <Stack.Screen 
+            name="MealsOverview" 
+            component={MealsOverViewScreen} 
+           // options={({route, navigation}) => {
+            //  const catId = route.params.categoryId;
+            //  return{
+            //    title: catId,
+            //  }
+            //}} 
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
@@ -23,7 +41,5 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    
-  },
+  container: {},
 });
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,14 +1,25 @@
 import { View, FlatList, Text, StyleSheet } from "react-native";
 import React from "react";
-import { MEALS } from "../data/dummy-data";
+import { useLayoutEffect } from "react";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 
-const MealsOverViewScreen = ({ route }) => {
+const MealsOverViewScreen = ({ route, navigation }) => {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find((category) => category.id === catId).title;
+  
+    navigation.setOptions({
+      title: categoryTitle
+    });
+
+  }, [catId, navigation]);
+
 
   function renderMealItem(itemData) {
     const item = itemData.item;
